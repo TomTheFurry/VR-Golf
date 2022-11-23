83e8f08aae64a725530f810a37aff74eb5031893
unknown <a1@tyit35656.vtc.hk>
2022-11-23 15:40:22 +0800

    Level 1

---
+++ Assets/Scenes/Ball.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rig;
+    Vector3 pos;
+    public float forceMin = 1800f;
+    public float forceMax = 2200f;
+
+    void Start()
+    {
+        pos = transform.position;
+        pos.z=  0;
+        transform.position = pos + Vector3.forward * Random.Range(-20, 20);
+        rig = GetComponent<Rigidbody>();
+        rig.AddForce(Vector3.right * -Random.Range(forceMin, forceMax));
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Goal");
+        transform.position = pos + Vector3.forward * Random.Range(-20, 20);
+        transform.rotation = Quaternion.identity;
+        rig.velocity = Vector3.zero;
+        rig.angularVelocity = Vector3.zero;
+        rig.AddForce(Vector3.right * -Random.Range(forceMin, forceMax));
+    }
+}
---
+++ Assets/Scenes/Goal.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public float timeScale = 1;
+
+    private void Update()
+    {
+        Time.timeScale = timeScale;
+    }
+}
