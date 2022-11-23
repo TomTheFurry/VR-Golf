72953e14974683ac5dc5e1a83f7c373b72bc2ec5
unknown <a1@tyit35656.vtc.hk>
2022-11-23 16:37:46 +0800

    level 1

--- Assets/Scenes/Ball.cs
+++ Assets/Scenes/Ball.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    Vector3 RandForce => Quaternion.AngleAxis(Random.Range(angleMin, angleMax), Vector3.up) * (Vector3.left * Random.Range(forceMin, forceMax));
+    Vector3 RandPos => pos + Vector3.forward * Random.Range(-20, 20);
+
     Rigidbody rig;
     Vector3 pos;
     public float forceMin = 1800f;
     public float forceMax = 2200f;
+    public float angleMin = -30f;
+    public float angleMax = 30f;
 
     void Start()
     {
         pos = transform.position;
         pos.z=  0;
-        transform.position = pos + Vector3.forward * Random.Range(-20, 20);
+        transform.position = RandPos;
         rig = GetComponent<Rigidbody>();
-        rig.AddForce(Vector3.right * -Random.Range(forceMin, forceMax));
+        rig.AddForce(RandForce);
+    }
+
+    private void Update() {
+        if (rig.IsSleeping()) {
+            rig.AddForce(RandForce);
+        }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Ball - Goal");
-        transform.position = pos + Vector3.forward * Random.Range(-20, 20);
+        transform.position = RandPos;
         transform.rotation = Quaternion.identity;
         rig.velocity = Vector3.zero;
         rig.angularVelocity = Vector3.zero;
-        rig.AddForce(Vector3.right * -Random.Range(forceMin, forceMax));
+        rig.AddForce(RandForce);
     }
 }
