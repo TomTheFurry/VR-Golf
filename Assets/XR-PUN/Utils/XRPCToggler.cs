747920e64a3569ce621a02933de4dc5131f7751d
unknown <a1@tyit35643.vtc.hk>
2022-12-12 17:01:42 +0800

    Merge

--- Assets/XR-PUN/Utils/XRPCToggler.cs
+++ Assets/XR-PUN/Utils/XRPCToggler.cs
@@ -3,15 +3,10 @@ public class XRPCToggler : XRToggler
     public bool OnlyOnInit = true;
     public new ToggleMode ToggleMode = ToggleMode.Destory;
 
-<<<<<<< HEAD
-    public override bool InitOnly { get; } = false;
-    public override ToggleMode Toggle { get; } = ToggleMode.Toggle;
-=======
     public bool PCMode = false;
 
     public override bool InitOnly => OnlyOnInit;
     public override ToggleMode Toggle => ToggleMode;
->>>>>>> 6955565a3ebc6c3bee8eff621efc8ea04b6427e5
 
     public XRPCToggler() : base()
     {
