5e5428845d13f6c6229466ee0424b27aef4952aa
unknown <a1@tyit35655.vtc.hk>
2022-12-14 14:07:46 +0800

    afs

--- Assets/XR-PUN/XRControl.cs
+++ Assets/XR-PUN/XRControl.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRControl : MonoBehaviour
 {
@@ -10,6 +12,9 @@ public class XRControl : MonoBehaviour
     public Transform MoveHandRef;
 
     public InputActionReference MoveAction;
+    public InputActionReference GrabMoveAction;
+
+    public XRRayInteractor Interactor;
 
     public float moveSpeed = 10f;
 
@@ -21,5 +26,17 @@ public class XRControl : MonoBehaviour
         var moveDir = yawRot * new Vector3(relMove.x, 0, relMove.y);
         var move = Vector3.Normalize(moveDir) * moveSpeed;
         XROrigin.position += move * Time.deltaTime;
+
+        var list = Interactor.interactablesSelected;
+        if (list.Count != 0 && list[0] is XRGrabInteractable grabbed) {
+            Vector3 anchorOffset = grabbed.attachTransform.localPosition;
+            Vector3 euler = grabbed.attachTransform.localEulerAngles;
+            
+            var grabMove = GrabMoveAction.action.ReadValue<Vector2>();
+            anchorOffset.z += grabMove.y * Time.deltaTime;
+            euler.z += grabMove.x * Time.deltaTime;
+            grabbed.attachTransform.localPosition = anchorOffset;
+            grabbed.attachTransform.localEulerAngles = euler;
+        }
     }
 }
